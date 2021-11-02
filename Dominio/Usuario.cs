3335ced7dda0c15973fc0cfb3ccdaa90c9fe8361
Se7enDoorS <christian.sepulveda@outlook.com>
2021-11-02 14:35:02 -0300

    Agregadas clases

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario:Persona
+    {
+        public TipoUsuario Tipo { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
