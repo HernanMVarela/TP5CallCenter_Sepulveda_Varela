22fa2ead32f7ff643fc71efb7acfb9c74ffab897
HernanMVarela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2021-11-09 00:14:42 -0300

    Ajustes varios y pagina nuevo ticket
    Arreglado bootstrap (con archivos locales)
    Avanzado nuevo ticket (layout y elementos)

--- TP5_CallCenter_Sepulveda_Varela/NuevoTicket.aspx
+++ TP5_CallCenter_Sepulveda_Varela/NuevoTicket.aspx
@@ -1,11 +1,56 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="NuevoTicket.aspx.cs" Inherits="TP5_CallCenter_Sepulveda_Varela.NuevoTicket" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="container-fluid cuadro_uno">
-        <asp:Label ID="lblDescripcion" runat="server" Text="Descripción"></asp:Label>
-        <asp:TextBox ID="txbDescripcion" Height="300px" TextMode="MultiLine" Wrap="true" runat="server"></asp:TextBox>
-
-
+    <div class="container-fluid cuadro_uno overflow-hidden" style="min-height:600px;">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <asp:Label ID="lblNuevoTicket" style="padding 10px; font-size:40px;" runat="server" Text="Nuevo ticket"></asp:Label>
+            </div>
+        </div>
+        <div class="row my-3 me-2">
+            <div class="col-2 d-flex justify-content-center align-items-center">
+                <asp:Label ID="lblDescripcion" style="padding 10px; font-size:30px;" runat="server" Text="Descripción"></asp:Label>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+                <asp:TextBox ID="txbDescripcion" class="w-100" style="min-height:150px; max-height:250px" MaxLength="350" TextMode="MultiLine" Wrap="true" runat="server"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row my-2 me-2">
+            <div class="col-2 d-flex justify-content-center align-items-center">   
+                <asp:Label ID="lblCliente" style="padding 10px; font-size:30px;" runat="server" Text="Cliente"></asp:Label>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+                <asp:TextBox ID="txbCliente" class="w-100" MaxLength="100" Wrap="false" runat="server"></asp:TextBox>
+            </div>
+            <div class="col-2 d-flex justify-content-center align-items-center">
+                <asp:Button ID="btnAgregarCliente" class="btn btn-primary" runat="server" Text="Agregar" />
+            </div>
+        </div>
+        <div class="row my-2 me-2">
+            <div class="col-2 d-flex justify-content-center align-items-center">   
+                <asp:Label ID="lblTecnico" style="padding 10px; font-size:30px;" runat="server" Text="Técnico"></asp:Label>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+                <asp:TextBox ID="txbTecnico" class="w-100" MaxLength="100" Wrap="false" runat="server"></asp:TextBox>
+            </div>
+            <div class="col-2 d-flex justify-content-center align-items-center">
+                <asp:Button ID="btnAgregarTecnico" class="btn btn-primary" runat="server" Text="Agregar" />
+            </div>
+        </div>
+        <div class="row mb-2 mt-3 me-2">
+            <div class="col-2 d-flex justify-content-center align-items-center">   
+                <asp:Label ID="lblComentario" style="padding 10px; font-size:30px;" runat="server" Text="Comentario"></asp:Label>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+                <asp:TextBox ID="txbComentario" class="w-100" style="min-height:75px; max-height:150px" MaxLength="150" TextMode="MultiLine" Wrap="true" runat="server"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row mb-2 mt-3 me-2">
+            <div class="col-12 d-flex justify-content-center align-self-end">
+                <p style="font-size:20px"><%: DateTime.Now %></p>
+            </div>
+       </div>
+        
     </div>
 
 </asp:Content>
--- TP5_CallCenter_Sepulveda_Varela/NuevoTicket.aspx.designer.cs
+++ TP5_CallCenter_Sepulveda_Varela/NuevoTicket.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace TP5_CallCenter_Sepulveda_Varela
     {
 
         /// <summary>
+        /// Control lblNuevoTicket.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNuevoTicket;
+
+        /// <summary>
         /// Control lblDescripcion.
         /// </summary>
         /// <remarks>
@@ -31,5 +40,77 @@ namespace TP5_CallCenter_Sepulveda_Varela
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txbDescripcion;
+
+        /// <summary>
+        /// Control lblCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCliente;
+
+        /// <summary>
+        /// Control txbCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbCliente;
+
+        /// <summary>
+        /// Control btnAgregarCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarCliente;
+
+        /// <summary>
+        /// Control lblTecnico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTecnico;
+
+        /// <summary>
+        /// Control txbTecnico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbTecnico;
+
+        /// <summary>
+        /// Control btnAgregarTecnico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarTecnico;
+
+        /// <summary>
+        /// Control lblComentario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblComentario;
+
+        /// <summary>
+        /// Control txbComentario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbComentario;
     }
 }
